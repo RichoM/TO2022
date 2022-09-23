63f6130e931575e669265fccd2d1e5d068ca65c8
RichoM <richi.moran@gmail.com>
2022-09-23 15:53:12 -0300

    Agrego ejemplo de paralelismo a nivel de instrucción

---
+++ Ejemplos/InstructionLevelParallelism/Program.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace InstructionLevelParallelism
+{
+    public class ILPBenchmark
+    {
+        [Benchmark]
+        public int Slow()
+        {
+            int s = 0;
+            for (int i = 0; i < 100000; i++)
+            {
+                s++; s++; s++; s++;
+            }
+            return s;
+        }
+
+        [Benchmark]
+        public int Fast()
+        {
+            int a = 0, b = 0, c = 0, d = 0;
+            for (int i = 0; i < 100000; i++)
+            {
+                a++; b++; c++; d++;
+            }
+            return a + b + c + d;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = new ILPBenchmark();
+            var slow = b.Slow();
+            var fast = b.Fast();
+            Console.WriteLine($"{slow} == {fast}");
+
+            var summary = BenchmarkRunner.Run<ILPBenchmark>();
+        }
+    }
+}
