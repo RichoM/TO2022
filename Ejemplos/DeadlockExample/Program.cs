4999551fd2449d8c6bba5801240b95f5dcf9f394
RichoM <richi.moran@gmail.com>
2022-11-04 21:39:30 -0300

    Agrego los ejemplos de race condition y deadlock vistos en clase

---
+++ Ejemplos/DeadlockExample/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace DeadlockExample
+{
+    internal class Program
+    {
+        static int counter = 0;
+        static object lock_1 = new object();
+        static object lock_2 = new object();
+
+        static void T1()
+        {
+            lock (lock_1)
+            {
+                Thread.Sleep(100);
+                lock (lock_2)
+                {
+                    counter = counter + 1;
+                }
+            }
+        }
+
+        static void T2()
+        {
+            lock (lock_1)
+            {
+                Thread.Sleep(100);
+                lock (lock_2)
+                {
+                    counter = counter + 1;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Va!");
+
+            var t1 = new Thread(T1);
+            var t2 = new Thread(T2);
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            Console.Write(counter);
+        }
+    }
+}
